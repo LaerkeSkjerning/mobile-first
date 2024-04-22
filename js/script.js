ce8663bb2762bda7dac540e5bf446a40fb915274
louisemogensenn <eaa23lmog@students.eaaa.dk>
2024-04-22 18:56:32 +0200

    Kommentarer i JS

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@
 /* ÅBNES SIDEN MED EN BREDDE PÅ 1024PX VIL DER VISES EN KARRUSEL OG DENNE KØRER STADIG SELVOM VINDUET GØRES STØRRE END 1024PX */
 /* ÅBNES SIDEN MED EN BREDDE, DER ER STØRRE END 1024PX KØRER KARRUSELLEN IKKE - AL JS VIL VÆRE DEAKTIVERET INDTIL FANEN ÅBNES IGEN */
 
-if (window.innerWidth < 1025){
+if (window.innerWidth < 1025){ /* If-statement, der frasorterer de skærme, der er større end 1024px. Ved en skærm, der er større end 1024px kører karrusellen ikke */
     document.addEventListener("DOMContentLoaded", function() { /* listener, der venter paa at al HTML-indholdet er loaded foer funktionen starter */
-        const inner = document.querySelector(".indre-karrusel"); /* querySelector henter det foerste element, der har klassen 'indre-karrusel' */
-        const pages = document.querySelectorAll(".karrusel-side"); /* querySelectorAll henter alle elementer med klassen 'karrusel-side' (og gemmer dem i en NodeList) */
-        const colors = ["var(--primaer-lyseroed)", "var(--primaer-gul)", "var(--primaer-lyseroed)"]; // Listen over baggrundsfarver
-        let currentIndex = 0; /* currentIndex skal aendre værdi og derfor kan variablen ikke vaere en const */
-        let direction = 1; /* direction skal aendre vaerdi og derfor kan variblen ikke vaere en const */
+        const inner = document.querySelector(".indre-karrusel"); /* querySelector henter det foerste element, der stemmer overens med argumentet - her er det klassen 'indre-karrusel' */
+        const pages = document.querySelectorAll(".karrusel-side"); /* querySelectorAll henter alle elementer med klassen 'karrusel-side' */
+        const colors = ["var(--primaer-lyseroed)", "var(--primaer-gul)", "var(--primaer-lyseroed)"]; /* Array indeholdende baggrundsfarverne. Der har hhv. indexvaerdierne 0, 1 og 2 */
+        let currentIndex = 0; /* currentIndex skal aendre værdi og variablen er derfor en let. Variablen bruges til at skifte side i karrusellen */
+        let direction = 1; /* direction skal aendre vaerdi og variablen er derfor en let. Variablen bruges til at skifte side i karrusellen */
 
         function showNextPage() { /* Funktion, der skal vise den naeste side */
             currentIndex += direction; /* Laegger vaerdien af direction til vaerdien for currentIndex (0 += 1) */
@@ -22,7 +22,7 @@ if (window.innerWidth < 1025){
 
         /* Denne funktion flytter karruselens indhold til den aktuelle side */
         function translatePages() {
-            const offset = -currentIndex * 100; /* En konstant offset faar en negativ vaerdi for at flytte indholdet til venstre. Vi ganger med 100 for at give indholdet en bestemt stoerrelse og paa den maade bestemme, hvor meget siden skal flytte sig for at skifte helt. Da hver side skal fylde 100% forskyder ved ogsaa med 100%*/
+            let offset = -currentIndex * 100; /* offset faar en negativ vaerdi for at flytte indholdet til venstre. Vi ganger med 100 for at give indholdet en bestemt stoerrelse og paa den maade bestemme, hvor meget siden skal flytte sig for at skifte helt. Da hver side skal fylde 100% forskyder ved ogsaa med 100%*/
             inner.style.transform = `translateX(${offset}%)`; /* Dette anvender det beregnede offset paa .indre-karrusel for at flytte siderne og lave karruselbevaegelsen. translateX tager et argument, der angiver den vandrette forskydning i % */
             changeBackgroundColor(); // Opdater baggrundsfarven - se nedstaaende
         }
@@ -37,3 +37,4 @@ if (window.innerWidth < 1025){
     });
 
 }
+
